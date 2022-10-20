b0757b85e8db38c1a1a3d0bbb88cde4c4d8487ff
Alex Trostanovsky <atrostan@cs.ubc.ca>
2022-10-20 11:15:58 -0700

    add size string to header; fix merge conflict in setup;

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from konect_scraper.sql import get_all_graphs_by_graph_names_where_stats_between
     get_all_unipartite_undirected_graphs
 from konect_scraper.util import \
     create_log_dir_if_not_exists, init_logger, valid_orderings, valid_pr, get_category, get_pr_struct_size, \
-    get_unimputed_features, get_directed, get_n, get_m, get_n_vertices, get_n_edges
+    get_unimputed_features, get_directed, get_n, get_m, get_n_vertices, get_n_edges, convert_size
 
 
 def main(args):
@@ -96,20 +96,19 @@ def main(args):
     print(f"{'Index' : <5} {'Graph Name' : <40}"
           f"{'|V|': <40}"
           f"{'|E|': <40}"
-          f"{'PageRank Struct Size': <40}"
+          f"{'PageRank Struct Size ': <40}"
           f"{'Graph Category': <40}"
           f"{'Is Directed?': <40}")
-    print('-' * 190)
+    print('-' * 245)
     for i, row in enumerate(rows):
-        # if int(get_directed(row['graph_name'])) == 1:
-        #     continue
-        print(f"{i : <5} {row['graph_name'] : <40}"
+        size, size_str = convert_size(get_pr_struct_size(row['graph_name']))
+        print(f"{i + graph_name_start_idx : <5} {row['graph_name'] : <40}"
               f"{get_n_vertices(row['graph_name']): <40}"
               f"{get_n_edges(row['graph_name']): <40}"
-              f"{get_pr_struct_size(row['graph_name']): <40}"
+              f"{size}" + f" {size_str: <40}"
               f"{get_category(row['graph_name']): <40}"
               f"{get_directed(row['graph_name']): <40}")
-    return
+    # return
     if download:
         download_and_extract.main(rows, io_modes)
     if orders:
